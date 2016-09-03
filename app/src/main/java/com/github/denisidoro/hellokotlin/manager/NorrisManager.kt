c9298a99c27ff0371d8fd2cf9a0f372d66c814ec
Denis Isidoro <den.isidoro@gmail.com>
2016-09-03 10:49:26 -0300

    Final touches

--- app/src/main/java/com/github/denisidoro/hellokotlin/manager/NorrisManager.kt
+++ app/src/main/java/com/github/denisidoro/hellokotlin/manager/NorrisManager.kt
@@ -6,12 +6,14 @@ import com.github.denisidoro.hellokotlin.utils.toObservable
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import rx.Single
+import java.util.concurrent.TimeUnit
 
 open class NorrisManager(val client: OkHttpClient, val gson: Gson) {
 
     open fun getJoke(id: Int): Single<Joke> = client
             .toObservable("http://api.icndb.com/jokes/$id")
             .parse(gson, Joke::class.java)
-            .toSingle();
+            .toSingle()
+            .delay(2500, TimeUnit.MILLISECONDS); // for demo purposes
 
 }
